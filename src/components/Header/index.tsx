e2817f73bb5b9b89a1adc2cb561df9f1ca76a297
Saivelin <vlad.pochkalov@mail.ru>
2022-06-16 11:48:06 +0300

    header + footer

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,22 +2,24 @@ import React from "react";
 import cl from "./header.module.css";
 import { FiChevronDown } from "react-icons/fi";
 import { IoSearchOutline } from "react-icons/io5"
+import { BsFillTelephoneFill } from "react-icons/bs"
+import { FiShoppingCart } from "react-icons/fi"
+import logo from "../../img/logo.png"
 
 const Header = () => {
     return (
         <header className={cl.header}>
-            <div className={cl.headerPromo}></div>
+            <div className={cl.headerPromo}><p>Cкидка 10% по промокоду "TEPLO" на все заказы до 10.09</p><span>Обратный звонок</span></div>
             <nav className={cl.nav}>
                 <ul className={cl.navList}>
-                    <li className={cl.navListItem}><img className={cl.navListLogo} src="" alt="" /></li>
+                    <li className={cl.navListItem}><img className={cl.navListLogo} src={logo} alt="" /></li>
                     <li className={cl.navListItem}>Главная</li>
                     <li className={cl.navListItem}>Каталог <FiChevronDown /></li>
                     <li className={cl.navListItem}>О нас</li>
                     <li className={cl.navListItem}>Еще <FiChevronDown /></li>
-                    <li className={cl.navListItem + " " + cl.search}><IoSearchOutline /><form action=""><input type="text" className={cl.navListItemInput} /><button>Поиск</button></form></li>
-                    <li className={cl.navListItem}></li>
-                    <li className={cl.navListItem}></li>
-                    <li className={cl.navListItem}></li>
+                    <li className={cl.navListItem + " " + cl.search}><IoSearchOutline className={cl.loupe} /><form action="" className={cl.form}><input placeholder="Поиск" type="text" className={cl.navListItemInput} /><button className={cl.navListItemBtn}>Найти</button></form></li>
+                    <li className={cl.navListItem + " " + cl.phoneItem}><div className={cl.phoneWrapper} ><BsFillTelephoneFill className={cl.phone} /></div><span>+7 (964) 945 41 38</span></li>
+                    <li className={cl.navListItem}><FiShoppingCart stroke="#F6AB3A" className={cl.yellow} /></li>
                 </ul>
             </nav>
         </header>
